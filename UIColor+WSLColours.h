9714c2edd27658095310cec1caea9e7499ab32c8
Stephen Darlington <stephen@zx81.org.uk>
2013-10-29 16:00:03 +0000

    Colours used in Wandle logo and Rootn Tootn

---
+++ UIColor+WSLColours.h
@@ -0,0 +1,34 @@
+//
+//  UIColor+WSLColours.h
+//  timesince
+//
+//  Created by Stephen Darlington on 26/08/2012.
+//  Copyright (c) 2012 Wandle Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (WSLColours)
+
+// Left to right on the logo
++ (UIColor*)WSLMidOrange;
++ (UIColor*)WSLMidGreen;
++ (UIColor*)WSLLightOrange;
++ (UIColor*)WSLOrange;
++ (UIColor*)WSLDarkOrange;
++ (UIColor*)WSLLightGreen;
++ (UIColor*)WSLLightBlue;
++ (UIColor*)WSLDarkBlue;
++ (UIColor*)WSLDarkGreen;
++ (UIColor*)WSLPurple;
++ (UIColor*)WSLOliveGreen;
+
+// Rootn Tootn colours
++ (UIColor*)WSLRTOrange;
++ (UIColor*)WSLRTYellow;
++ (UIColor*)WSLRTCyan;
++ (UIColor*)WSLLightPurple;
++ (UIColor*)WSLRTLightBlue;
++ (UIColor*)WSLRTLightGreen;
+
+@end
---
+++ UIColor+WSLColours.m
@@ -0,0 +1,81 @@
+//
+//  UIColor+WSLColours.m
+//  timesince
+//
+//  Created by Stephen Darlington on 26/08/2012.
+//  Copyright (c) 2012 Wandle Software Limited. All rights reserved.
+//
+
+#import "UIColor+WSLColours.h"
+
+@implementation UIColor (WSLColours)
+
++ (UIColor*)WSLMidOrange {
+    return [UIColor colorWithRed:225.0/255.0 green:133.0/255.0 blue:77.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLMidGreen {
+    return [UIColor colorWithRed:0.0 green:110.0/255.0 blue:70.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLLightOrange {
+    return [UIColor colorWithRed:251.0/255.0 green:194.0/255.0 blue:94.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLOrange {
+    return [UIColor colorWithRed:234.0/255.0 green:119.0/255.0 blue:64.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLDarkOrange {
+    return [UIColor colorWithRed:211.0/255.0 green:84.0/255.0 blue:54.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLLightGreen {
+    return [UIColor colorWithRed:210.0/255.0 green:218.0/255.0 blue:73.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLLightBlue {
+    return [UIColor colorWithRed:26.0/255.0 green:116.0/255.0 blue:181.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLDarkBlue {
+    return [UIColor colorWithRed:44.0/255.0 green:61.0/255.0 blue:138.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLDarkGreen {
+    return [UIColor colorWithRed:0.0 green:94.0/255.0 blue:47.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLPurple {
+    return [UIColor colorWithRed:105.0/255.0 green:45.0/255.0 blue:73.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLOliveGreen {
+    return [UIColor colorWithRed:126.0/255.0 green:160.0/255.0 blue:74.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLRTOrange {
+    return [UIColor colorWithRed:249.0/255.0 green:158.0/255.0 blue:127.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLRTYellow {
+    return [UIColor colorWithRed:218.0/255.0 green:222.0/255.0 blue:75.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLRTCyan {
+    return [UIColor colorWithRed:96.0/255.0 green:198.0/255.0 blue:196.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLLightPurple {
+    return [UIColor colorWithRed:155.0/255.0 green:161.0/255.0 blue:195.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLRTLightBlue {
+    return [UIColor colorWithRed:209.0/255.0 green:232.0/255.0 blue:238.0/255.0 alpha:1.0];
+}
+
++ (UIColor*)WSLRTLightGreen {
+    return [UIColor colorWithRed:147.0/255.0 green:211.0/255.0 blue:182.0/255.0 alpha:1.0];
+}
+
+@end
