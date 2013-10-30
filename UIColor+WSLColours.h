d23c822c4717af4296199c18a0977b5acbebfccf
Stephen Darlington <stephen@zx81.org.uk>
2013-10-30 12:51:13 +0000

    Create a class extension for a fake class called WSLColor — allows it to work on iOS and Mac.

--- UIColor+WSLColours.h
+++ UIColor+WSLColours.h
@@ -6,29 +6,42 @@
 //  Copyright (c) 2012 Wandle Software Limited. All rights reserved.
 //
 
+// Make it work on both iOS and Mac OS X
+#if TARGET_OS_IPHONE
+// iOS device
 #import <UIKit/UIKit.h>
+#define WSLColor UIColor
+#elif TARGET_OS_MAC
+// Mac OS
+#import <Cocoa/Cocoa.h>
+#define WSLColor NSColor
+#else
+#error Unsupported platform
+#endif
 
-@interface UIColor (WSLColours)
+// At compile time this will be a class extension to UIColor or NSColor
+// depending on the platform it's being compiled for.
+@interface WSLColor (WSLColours)
 
 // Left to right on the logo
-+ (UIColor*)WSLMidOrange;
-+ (UIColor*)WSLMidGreen;
-+ (UIColor*)WSLLightOrange;
-+ (UIColor*)WSLOrange;
-+ (UIColor*)WSLDarkOrange;
-+ (UIColor*)WSLLightGreen;
-+ (UIColor*)WSLLightBlue;
-+ (UIColor*)WSLDarkBlue;
-+ (UIColor*)WSLDarkGreen;
-+ (UIColor*)WSLPurple;
-+ (UIColor*)WSLOliveGreen;
++ (WSLColor*)WSLMidOrange;
++ (WSLColor*)WSLMidGreen;
++ (WSLColor*)WSLLightOrange;
++ (WSLColor*)WSLOrange;
++ (WSLColor*)WSLDarkOrange;
++ (WSLColor*)WSLLightGreen;
++ (WSLColor*)WSLLightBlue;
++ (WSLColor*)WSLDarkBlue;
++ (WSLColor*)WSLDarkGreen;
++ (WSLColor*)WSLPurple;
++ (WSLColor*)WSLOliveGreen;
 
 // Rootn Tootn colours
-+ (UIColor*)WSLRTOrange;
-+ (UIColor*)WSLRTYellow;
-+ (UIColor*)WSLRTCyan;
-+ (UIColor*)WSLLightPurple;
-+ (UIColor*)WSLRTLightBlue;
-+ (UIColor*)WSLRTLightGreen;
++ (WSLColor*)WSLRTOrange;
++ (WSLColor*)WSLRTYellow;
++ (WSLColor*)WSLRTCyan;
++ (WSLColor*)WSLLightPurple;
++ (WSLColor*)WSLRTLightBlue;
++ (WSLColor*)WSLRTLightGreen;
 
 @end
--- UIColor+WSLColours.m
+++ UIColor+WSLColours.m
@@ -8,74 +8,74 @@
 
 #import "UIColor+WSLColours.h"
 
-@implementation UIColor (WSLColours)
+@implementation WSLColor (WSLColours)
 
-+ (UIColor*)WSLMidOrange {
-    return [UIColor colorWithRed:225.0/255.0 green:133.0/255.0 blue:77.0/255.0 alpha:1.0];
++ (WSLColor*)WSLMidOrange {
+    return [WSLColor colorWithRed:225.0/255.0 green:133.0/255.0 blue:77.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLMidGreen {
-    return [UIColor colorWithRed:0.0 green:110.0/255.0 blue:70.0/255.0 alpha:1.0];
++ (WSLColor*)WSLMidGreen {
+    return [WSLColor colorWithRed:0.0 green:110.0/255.0 blue:70.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLLightOrange {
-    return [UIColor colorWithRed:251.0/255.0 green:194.0/255.0 blue:94.0/255.0 alpha:1.0];
++ (WSLColor*)WSLLightOrange {
+    return [WSLColor colorWithRed:251.0/255.0 green:194.0/255.0 blue:94.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLOrange {
-    return [UIColor colorWithRed:234.0/255.0 green:119.0/255.0 blue:64.0/255.0 alpha:1.0];
++ (WSLColor*)WSLOrange {
+    return [WSLColor colorWithRed:234.0/255.0 green:119.0/255.0 blue:64.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLDarkOrange {
-    return [UIColor colorWithRed:211.0/255.0 green:84.0/255.0 blue:54.0/255.0 alpha:1.0];
++ (WSLColor*)WSLDarkOrange {
+    return [WSLColor colorWithRed:211.0/255.0 green:84.0/255.0 blue:54.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLLightGreen {
-    return [UIColor colorWithRed:210.0/255.0 green:218.0/255.0 blue:73.0/255.0 alpha:1.0];
++ (WSLColor*)WSLLightGreen {
+    return [WSLColor colorWithRed:210.0/255.0 green:218.0/255.0 blue:73.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLLightBlue {
-    return [UIColor colorWithRed:26.0/255.0 green:116.0/255.0 blue:181.0/255.0 alpha:1.0];
++ (WSLColor*)WSLLightBlue {
+    return [WSLColor colorWithRed:26.0/255.0 green:116.0/255.0 blue:181.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLDarkBlue {
-    return [UIColor colorWithRed:44.0/255.0 green:61.0/255.0 blue:138.0/255.0 alpha:1.0];
++ (WSLColor*)WSLDarkBlue {
+    return [WSLColor colorWithRed:44.0/255.0 green:61.0/255.0 blue:138.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLDarkGreen {
-    return [UIColor colorWithRed:0.0 green:94.0/255.0 blue:47.0/255.0 alpha:1.0];
++ (WSLColor*)WSLDarkGreen {
+    return [WSLColor colorWithRed:0.0 green:94.0/255.0 blue:47.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLPurple {
-    return [UIColor colorWithRed:105.0/255.0 green:45.0/255.0 blue:73.0/255.0 alpha:1.0];
++ (WSLColor*)WSLPurple {
+    return [WSLColor colorWithRed:105.0/255.0 green:45.0/255.0 blue:73.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLOliveGreen {
-    return [UIColor colorWithRed:126.0/255.0 green:160.0/255.0 blue:74.0/255.0 alpha:1.0];
++ (WSLColor*)WSLOliveGreen {
+    return [WSLColor colorWithRed:126.0/255.0 green:160.0/255.0 blue:74.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLRTOrange {
-    return [UIColor colorWithRed:249.0/255.0 green:158.0/255.0 blue:127.0/255.0 alpha:1.0];
++ (WSLColor*)WSLRTOrange {
+    return [WSLColor colorWithRed:249.0/255.0 green:158.0/255.0 blue:127.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLRTYellow {
-    return [UIColor colorWithRed:218.0/255.0 green:222.0/255.0 blue:75.0/255.0 alpha:1.0];
++ (WSLColor*)WSLRTYellow {
+    return [WSLColor colorWithRed:218.0/255.0 green:222.0/255.0 blue:75.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLRTCyan {
-    return [UIColor colorWithRed:96.0/255.0 green:198.0/255.0 blue:196.0/255.0 alpha:1.0];
++ (WSLColor*)WSLRTCyan {
+    return [WSLColor colorWithRed:96.0/255.0 green:198.0/255.0 blue:196.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLLightPurple {
-    return [UIColor colorWithRed:155.0/255.0 green:161.0/255.0 blue:195.0/255.0 alpha:1.0];
++ (WSLColor*)WSLLightPurple {
+    return [WSLColor colorWithRed:155.0/255.0 green:161.0/255.0 blue:195.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLRTLightBlue {
-    return [UIColor colorWithRed:209.0/255.0 green:232.0/255.0 blue:238.0/255.0 alpha:1.0];
++ (WSLColor*)WSLRTLightBlue {
+    return [WSLColor colorWithRed:209.0/255.0 green:232.0/255.0 blue:238.0/255.0 alpha:1.0];
 }
 
-+ (UIColor*)WSLRTLightGreen {
-    return [UIColor colorWithRed:147.0/255.0 green:211.0/255.0 blue:182.0/255.0 alpha:1.0];
++ (WSLColor*)WSLRTLightGreen {
+    return [WSLColor colorWithRed:147.0/255.0 green:211.0/255.0 blue:182.0/255.0 alpha:1.0];
 }
 
 @end
